6b13d0fd69a20788f741dd4c2712fc1ab079ad71
Abdullah Osama <abdalaosamah@gmail.com>
2021-03-22 04:08:34 +0200

    - First Commit

---
+++ js/main.js
@@ -0,0 +1,109 @@
+
+// Code Written in 2021 By abdullah Osama (abdalaosamah@gmail.com/owner@3bdallah.net)
+// -------------------------------------------------------------------------------------------------
+
+const DefaultReplacmentLetter = "س" // this is the letter to replace the first letter with 
+var Mode = 1 // 1 means arabic to sanguagelemon, 2 is the reverse
+
+function Lamoon(scentence){ // Converts The { scentence } to Sanguage Lemon
+    let words = scentence.split(' ')
+    let result = []
+
+    for(let i =0; i < words.length;i++){
+        if(isValid( words[i] )){
+            try {
+                if(words[i].substr(0,2) == "ال"){
+                    words[i] = words[i].substr(2)
+
+                let word = randomWord(words[i].charAt(0))
+                changed = words[i].split("")
+                changed[0] = DefaultReplacmentLetter
+                result.push("ال"+changed.join(""))
+                result.push( word )
+                }else{
+                    let word = randomWord(words[i].charAt(0))
+                changed = words[i].split("")
+                changed[0] = DefaultReplacmentLetter
+                result.push(changed.join(""))
+                result.push( word )
+                }
+                
+            } catch (error) {
+                alert(" حصل خطأ, رجاء مراجعه المدخلات")
+            }
+             }else{
+            result.push(words[i])
+        }
+        
+    }
+    return result.join(' ')
+}
+
+
+
+function randomWord(Letter){// Returns a random Word Starting With the {Letter}
+
+    return Words[Letter][~~(Math.random() * Words[Letter].length)];
+}
+
+function unLamoon(scentence) {
+    let words = scentence.split(' ')
+    let result = []
+    for(let i =0; i < words.length; i = i +2){
+        if(words[i].substr(0,2) == "ال"){
+            words[i] = words[i].substr(2)
+            let word = words[i].split('')
+            word[0] = words[i + 1].charAt(0)
+            result.push("ال"+word.join(''))
+        }else{
+            let word = words[i].split('')
+            word[0] = words[i + 1].charAt(0)
+            result.push(word.join(''))
+        }
+    }
+    return result.join(' ')
+}
+
+function isValid(Word){ // returns True if the word is valid and Lammonable :)
+
+    if(Word < 1) return false
+    if(Word == "و") return false
+    if((/[^ء-ي]/g).test(Word)) return false
+    return true
+    
+
+}
+
+// -------------------------------------------------------------------------------------------------
+// Events
+document.getElementById('convertBtn').addEventListener('click',function (e){
+    let text = document.getElementById('Input').value
+    if(/[a-z]/g.test(text)){
+        AlertError("حصل خطأ , راجع المدخلات")
+        return
+    }
+    
+    if(Mode == 1){
+        document.getElementById('out').innerHTML = Lamoon(text)
+    }else{
+        document.getElementById('out').innerHTML = unLamoon(text)
+    }
+
+    
+})
+function AlertError(Word){
+    alert(Word)
+    document.getElementById('Input').value = ""
+
+}
+document.querySelector("select").addEventListener('change',function(e) {
+   Mode = this.value
+   if(Mode == 1) {
+    document.getElementById('firstField').innerHTML = "عربي"
+    document.getElementById('secondField').innerHTML = "سغه لمون"
+   }else{
+    document.getElementById('firstField').innerHTML  = "سغه لمون"
+    document.getElementById('secondField').innerHTML = "عربي"
+   }
+   
+});
